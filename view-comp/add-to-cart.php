18542bc773d22e49236646893c84ceabda76b8c8
thunderdaawn <jeanmichelc.gomez@gmail.com>
2020-05-26 20:57:47 +0800

    updated logs

--- view-comp/add-to-cart.php
+++ view-comp/add-to-cart.php
@@ -3,12 +3,3 @@
 	<input type="number" class="store-item-quantity-input form-control col-4" min=1>
 	<button class="btn mx-3" name="add-to-cart-btn" style="background-color: #676299; color: white;" onclick="addToCartClicked()"><b>ADD TO CART</b></button>
 </div>
-
-<!-- <?php // @tian pls update :)
-
-	// if(isset($_POST['add-to-cart-btn'])) {
-		// addToCartLogMessage($_SESSION['username']); 
-	}
-
-?>
- -->
