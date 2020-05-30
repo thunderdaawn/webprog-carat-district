fdac794949b7d448f52d54c048e06bf2ab4f813c
thunderdaawn <jeanmichelc.gomez@gmail.com>
2020-05-30 18:02:43 +0800

    updated register

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php 
-if($_SERVER["HTTPS"] != "on"){
-header("Location: https://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"]);
-exit;
+	if($_SERVER["HTTPS"] != "on"){
+	header("Location: https://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"]);
+	exit;
 }
  ?>
 <!DOCTYPE html>
